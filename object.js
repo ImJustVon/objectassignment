f2f42d80f045e4669fa3e592cf22fc3692a676d0
Vaughn Prosser <vaughnprosser@gmail.com>
2016-09-22 10:10:42 -0500

    add getter and setter

--- object.js
+++ object.js
@@ -19,6 +19,24 @@ var tsm = {
      var info = 'Team name: ' + this.teamName + '\nCreated: ' + this.created + '\nGame played: ' + this.gameType + '\nSport type: ' + this.sport + '\nLeague: ' + this.league + '\nRanking: ' + this.ranking + '\nWin-Loss: ' + this.winLossRecord + '\nOwner: ' + this.owner + '\nHead Coach: ' + this.headCoach + '\nStrategic Coach: ' + this.strategicCoach + '\nNumber of players: ' + this.numberOfPlayers + '\nTop Lane: ' +this.topLane + '\nJungle: ' + this.jungle + '\nMid Lane: ' + this.midLane + '\nADC: ' + this.adc + '\nSupport: ' + this.support;
      return info;
   }
-
+  setTopLane: function(newTopLane) {
+    this.toplane = newTopLane;
+  }
+  setJungle: function(newJungle) {
+    this.jungle = newJungle;
+  }
+  setMidLane: function(newMidLane) {
+    this.midLane = newMidLane;
+  }
+  setADC: function(newADC) {
+    this.adc = newADC;
+  }
+  setSupport: function(newSupport) {
+    this.support = newSupport;
+  }
+  getCreatedAsNumber: function() {
+    return parseInt(this.created);
+  }
 }
+
 console.log(tsm.teamInfo());
